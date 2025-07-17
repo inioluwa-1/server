d0eb135c118c2acd3ad27f9690b21f9b5b74200a
ayanbisi inioluwa <ifeini048@gmail.com>
2025-07-17 10:16:10 +0100

    done

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -1,10 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
-    cloud_name: "dtwe3klnh",
-    api_key: "558726447181529",
-    api_secret: "PihKrlDOjNiaqDXCICzQYHq_jG0",
-})
-
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
+});
 
 const uploadFile = (req, res) => {
     let media = req.body.file;
