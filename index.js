b7a7949ab5c0be10f4cc4d1a81a9b773b89d7d66
ayanbisi inioluwa <ifeini048@gmail.com>
2025-07-17 10:42:15 +0100

    fix: correct path to UserRouter in index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = 3000;
-const UserRouter = require('./server/Routes/user.route');
+const UserRouter = require('./Routes/user.route');
 const cors = require('cors')
-
 app.use(cors())
 app.use(express.json({limit: '500mb'}));
 app.use(express.urlencoded({limit: '500mb', extended: true}));
